35fc34a9ca4651587f2b620185e776661bfcd72e
Kae <kaehehehe@gmail.com>
2022-04-12 22:10:00 +0900

    :sparkles: router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-//@ts-ignore
-import { GlobalStyle } from './GlobalStyle.ts';
+import { GlobalStyle } from './GlobalStyle';
+import Home from './pages/Home';
+import Issue from './pages/Issue';
+
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/issue" element={<Issue />} />
+        </Routes>
+      </Router>
     </>
   );
 };

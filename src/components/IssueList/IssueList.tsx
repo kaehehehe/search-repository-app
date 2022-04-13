8ca4bd0ea910b2bdf3c1c5fa4a00e955ad8bbeb5
Kae <kaehehehe@gmail.com>
2022-04-14 03:32:50 +0900

    :sparkles: issues가 카드 형태로 표시되도록 구현

---
+++ src/components/IssueList/IssueList.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import * as S from './style';
+import { IssueType } from '../../pages/Issue/Issue';
+import { formatDate } from '../../utils/formatDate';
+
+type IssueListProps = {
+  issues: IssueType[];
+  repoName: string | undefined;
+};
+
+const IssueList = ({ issues, repoName }: IssueListProps) => {
+  return (
+    <S.IssueList>
+      {issues.length === 0 ? (
+        <S.NoIssue>issue가 없습니다</S.NoIssue>
+      ) : (
+        issues.map(
+          ({ id, title, updated_at, html_url, author, author_avatar }) => (
+            <S.IssueCard key={id} href={html_url} target="_blank">
+              <S.CardTitleAndRepoName>
+                <h3>{title}</h3>
+                <span>{repoName}</span>
+              </S.CardTitleAndRepoName>
+              <S.AuthorData>
+                <img src={author_avatar} alt="이슈를 만든 사람의 사진" />
+                <span>{author}</span>
+              </S.AuthorData>
+              <S.UpdatedAt>{formatDate(updated_at)}</S.UpdatedAt>
+            </S.IssueCard>
+          )
+        )
+      )}
+    </S.IssueList>
+  );
+};
+
+export default IssueList;
--- src/pages/Issue/Issue.tsx
+++ src/pages/Issue/Issue.tsx
@@ -1,38 +1,66 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 import * as S from './style';
+import IssueList from '../../components/IssueList';
 
-type IssueType = {
+export type IssueType = {
   id: number;
+  updated_at: string;
   title: string;
+  html_url: string;
+  author: string;
+  author_avatar: string;
 };
 
 const Issue = () => {
   const [issues, setIssues] = useState<IssueType[]>([]);
   const [currPage, setCurrPage] = useState(1);
   const { owner, name } = useParams();
+  const navigate = useNavigate();
 
   const fetchIssues = async () => {
     return await axios.get(
-      `https://api.github.com/repos/${owner}/${name}/issues?page=${currPage}`
+      `https://api.github.com/repos/${owner}/${name}/issues?&page=${currPage}`
     );
   };
 
   useEffect(() => {
     fetchIssues()
-      .then((res) => setIssues(res.data))
+      .then((res) => {
+        console.log(res.data);
+        const issues = res.data;
+        console.log(issues);
+        const result = issues.map((issue: IssueType) => {
+          return {
+            id: issue.id,
+            updated_at: issue.updated_at,
+            title: issue.title,
+            html_url: issue.html_url,
+            // @ts-ignore
+            author: issue.user.login,
+            // @ts-ignore
+            author_avatar: issue.user.avatar_url,
+          };
+        });
+        setIssues(result);
+      })
       .catch((res) => console.error(res));
   }, []);
-  
+
   return (
     <S.Issue>
-      {issues.map(({ id, title }) => (
-        <div key={id}>
-          id: {id} title{title}
-        </div>
-      ))}
+      <S.HomeIcon
+        onClick={() => navigate('/')}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+      >
+        <path d="M575.8 255.5C575.8 273.5 560.8 287.6 543.8 287.6H511.8L512.5 447.7C512.5 450.5 512.3 453.1 512 455.8V472C512 494.1 494.1 512 472 512H456C454.9 512 453.8 511.1 452.7 511.9C451.3 511.1 449.9 512 448.5 512H392C369.9 512 352 494.1 352 472V384C352 366.3 337.7 352 320 352H256C238.3 352 224 366.3 224 384V472C224 494.1 206.1 512 184 512H128.1C126.6 512 125.1 511.9 123.6 511.8C122.4 511.9 121.2 512 120 512H104C81.91 512 64 494.1 64 472V360C64 359.1 64.03 358.1 64.09 357.2V287.6H32.05C14.02 287.6 0 273.5 0 255.5C0 246.5 3.004 238.5 10.01 231.5L266.4 8.016C273.4 1.002 281.4 0 288.4 0C295.4 0 303.4 2.004 309.5 7.014L564.8 231.5C572.8 238.5 576.9 246.5 575.8 255.5L575.8 255.5z" />
+      </S.HomeIcon>
+      <S.IssueTitle>{`${owner}/${name}`} ISSUES</S.IssueTitle>
+      <IssueList issues={issues} repoName={name} />
     </S.Issue>
   );
 };

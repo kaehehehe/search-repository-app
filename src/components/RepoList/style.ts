2de000d5e1afa7ba3ca54f7edb439d9e02a332b2
Kae <kaehehehe@gmail.com>
2022-04-13 18:52:45 +0900

    :lipstick: ListItem 스타일링 추가

--- src/components/RepoList/style.ts
+++ src/components/RepoList/style.ts
@@ -1,1 +1,18 @@
 import styled from 'styled-components';
+
+export const Message = styled.li`
+  font-size: 1.8rem;
+  color: #acbac7;
+`;
+
+export const ListItem = styled.li`
+  font-size: 1.8rem;
+  color: #539bf5;
+  padding: 8px 0;
+  transition: all 300ms linear;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;

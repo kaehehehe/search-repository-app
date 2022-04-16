8a23af41aeb459869032399523e51ff944288460
Kae <kaehehehe@gmail.com>
2022-04-16 17:02:11 +0900

    :sparkles: pagination 구현

--- src/pages/Issue/Issue.tsx
+++ src/pages/Issue/Issue.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 import * as S from './style';
 import IssueList from '../../components/IssueList';
 import Loading from '../../components/Loading';
+import { RepoType } from '../Home/Home';
 
 export type IssueType = {
   id: number;
@@ -16,16 +18,20 @@ export type IssueType = {
   author_avatar: string;
 };
 
-const Issue = () => {
+type IssueProps = {
+  savedRepos: RepoType[];
+};
+
+const Issue = ({ savedRepos }: IssueProps) => {
   const [issues, setIssues] = useState<IssueType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [currPage, setCurrPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const { owner, name } = useParams();
   const navigate = useNavigate();
 
-  const fetchIssues = async () => {
+  const fetchIssues = async (page: number = 1) => {
     return await axios.get(
-      `https://api.github.com/repos/${owner}/${name}/issues?&page=${currPage}`
+      `https://api.github.com/repos/${owner}/${name}/issues?&page=${page}`
     );
   };
 
@@ -35,15 +41,15 @@ const Issue = () => {
       .then((res) => {
         const issues = res.data;
         const result = issues.map((issue: IssueType) => {
+          // @ts-ignore
+          const { login, avatar_url } = issue.user;
           return {
             id: issue.id,
             updated_at: issue.updated_at,
             title: issue.title,
             html_url: issue.html_url,
-            // @ts-ignore
-            author: issue.user.login,
-            // @ts-ignore
-            author_avatar: issue.user.avatar_url,
+            author: login,
+            author_avatar: avatar_url,
           };
         });
         setIssues(result);
@@ -51,6 +57,38 @@ const Issue = () => {
       })
       .catch((res) => console.error(res));
   }, []);
+
+  const calculatePageCount = () => {
+    const targetRepo = savedRepos.filter(
+      (repo) => repo.full_name === `${owner}/${name}`
+    );
+    return Math.ceil(targetRepo[0].open_issues / 30);
+  };
+
+  const handlePaginate = (clickedPage: { selected: number }) => {
+    const page = clickedPage.selected;
+    setCurrentPage(page);
+    setIsLoading(true);
+    fetchIssues(page + 1)
+      .then((res) => {
+        const issues = res.data;
+        const result = issues.map((issue: IssueType) => {
+          // @ts-ignore
+          const { login, avatar_url } = issue.user;
+          return {
+            id: issue.id,
+            updated_at: issue.updated_at,
+            title: issue.title,
+            html_url: issue.html_url,
+            author: login,
+            author_avatar: avatar_url,
+          };
+        });
+        setIssues(result);
+        setIsLoading(false);
+      })
+      .catch((res) => console.error(res));
+  };
 
   return (
     <S.Issue>
@@ -63,6 +101,28 @@ const Issue = () => {
       </S.HomeIcon>
       <S.IssueTitle>{`${owner}/${name}`} ISSUES</S.IssueTitle>
       {isLoading ? <Loading /> : <IssueList issues={issues} repoName={name} />}
+      {!isLoading && (
+        <S.Paginate>
+          <ReactPaginate
+            forcePage={currentPage}
+            pageCount={calculatePageCount()}
+            onPageChange={handlePaginate}
+            marginPagesDisplayed={4}
+            previousLabel="<"
+            nextLabel=">"
+            breakLabel="..."
+            containerClassName="pagination justify-center"
+            pageClassName="page-item"
+            activeClassName="active"
+            activeLinkClassName="active"
+            previousClassName="page-item"
+            nextClassName="page-item"
+            disabledClassName="disabled-button d-none"
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+          />
+        </S.Paginate>
+      )}
     </S.Issue>
   );
 };
--- src/pages/Issue/style.ts
+++ src/pages/Issue/style.ts
@@ -23,3 +23,35 @@ export const IssueTitle = styled.h1`
   color: #acbac7;
 `;
 
+export const Paginate = styled.div`
+  margin: 30px 0;
+
+  .pagination {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  .page-item,
+  .page-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    width: 50px;
+    font-size: 25px;
+    font-weight: 700;
+    color: #acbac7;
+    margin-right: 6px;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+
+  .active {
+    color: #539bf5;
+  }
+`;

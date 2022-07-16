fd9093aee21fbfedb5a8f29a5dffba5dc23d48c0
Kae <kaehehehe@gmail.com>
2022-07-16 16:59:45 +0900

    :recycle: dayjs 적용

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,17 +1,6 @@
+import dayjs from 'dayjs';
+
 export const formatDate = (date: string) => {
-  const DATE = new Date(date);
-  const YEAR = DATE.getFullYear();
-  const MONTH = DATE.getMonth();
-  const DAY = DATE.getDate();
-  const HOURS = DATE.getHours();
-  const MINUTES = DATE.getMinutes();
-  const SECONDS = DATE.getSeconds();
-
-  const month = MONTH + 1 < 10 ? `0${MONTH}` : MONTH;
-  const day = DAY < 10 ? `0${DAY}` : DAY;
-  const hours = HOURS < 10 ? `0${HOURS}` : HOURS;
-  const minutes = MINUTES < 10 ? `0${MINUTES}` : MINUTES;
-  const seconds = SECONDS < 10 ? `0${SECONDS}` : SECONDS;
-
-  return `${YEAR}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  const d = dayjs(date);
+  return d.format('YYYY-MM-DD HH:mm:ss');
 };
